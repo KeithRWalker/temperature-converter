a6df0705f96c4271b29ffaa5d613d370fc443c61
Keith Walker <Keithrwalker418@gmail.com>
2019-03-17 22:28:03 -0500

    Conversion box works, and prints

--- main.js
+++ main.js
@@ -7,25 +7,45 @@ const fBtn = document.getElementById('fBtn');
 const tempOutput = document.getElementById('tempOutput');
 const tempInput = document.getElementById('tempInput');
 
-const determineConverter = () => {
+const determineConverter = () => {//This decides if you clicked C or F and runs accordingly
     if(cBtn.checked === true){
-        console.log(`${tempInput.value}C°`);
-        printToDom('tempOutput', `${tempInput.value}C°`);
+        toC(tempInput.value);
     } else if(fBtn.checked === true){
-        console.log(`${tempInput.value}F°`);
-        printToDom('tempOutput', `${tempInput.value}F°`);
-    } else{
-        console.log('Please Select a Unit');
+        toF(tempInput.value);
     };
 };
 
-const printToDom = (divId, textToPrint) => {
+const printToDom = (divId, textToPrint) => {//This is a standard print function
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
 
-convertBtn.addEventListener('click', determineConverter);
+const toC = (temp) => {// C = (F -32) / 1.8
+    const a = parseFloat(temp - 32)/1.8;
+    const roundedA = Math.round(a);
+    domStringBuilder(roundedA, "C");
+};
 
+const toF = (temp) => {// F = (C * 1.8) + 32
+    const a = parseFloat(temp * 1.8) + 32;
+    const roundedA = Math.round(a);
+    domStringBuilder(roundedA, "F");
+};
+
+const domStringBuilder = (finalTemp, unit) => {//This builds the string to type back to you
+    const domString = `<h2>${finalTemp} degrees ${unit}</h2>`;
+    printToDom('tempOutput', domString);
+};
+
+const eventListeners = () => {
+    convertBtn.addEventListener('click', determineConverter);
+};
+
+const init = () => {
+    eventListeners();
+};
+
+init();
 
 
 
