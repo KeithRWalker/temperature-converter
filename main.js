20ed2a76baec248d8897ebe1f35dd4c7338419fe
Keith Walker <Keithrwalker418@gmail.com>
2019-03-17 23:41:59 -0500

    clear button working

--- main.js
+++ main.js
@@ -2,23 +2,43 @@
 // F = (C * 1.8) + 32
 // 32F = 0
 const convertBtn = document.getElementById('convertBtn');
+const clearBtn = document.getElementById('clearBtn');
 const cBtn = document.getElementById('cBtn');
 const fBtn = document.getElementById('fBtn');
 const tempOutput = document.getElementById('tempOutput');
 const tempInput = document.getElementById('tempInput');
 
-const determineConverter = () => {//This decides if you clicked C or F and runs accordingly
-    if(cBtn.checked === true){
-        toC(tempInput.value);
-    } else if(fBtn.checked === true){
-        toF(tempInput.value);
-    };
+const init = () => {
+    eventListeners();
 };
 
 const printToDom = (divId, textToPrint) => {//This is a standard print function
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
 };
+
+const eventListeners = () => {
+    convertBtn.addEventListener('click', determineConverter);
+    clearBtn.addEventListener('click', clear);
+};
+
+
+const determineConverter = () => {//This decides if you clicked C or F and runs accordingly
+    if(cBtn.checked === true){
+        toC(tempInput.value);
+    } else if(fBtn.checked === true){
+        toF(tempInput.value);
+    } else{
+        console.log('Please Select a Unit');
+    };
+};
+
+const clear = () => {
+    const cleared = ' ';
+    tempInput.value = 0;
+    printToDom('tempOutput', cleared);
+};
+
 
 const toC = (temp) => {// C = (F -32) / 1.8
     const a = parseFloat(temp - 32)/1.8;
@@ -35,14 +55,6 @@ const toF = (temp) => {// F = (C * 1.8) + 32
 const domStringBuilder = (finalTemp, unit) => {//This builds the string to type back to you
     const domString = `<h2>${finalTemp} degrees ${unit}</h2>`;
     printToDom('tempOutput', domString);
-};
-
-const eventListeners = () => {
-    convertBtn.addEventListener('click', determineConverter);
-};
-
-const init = () => {
-    eventListeners();
 };
 
 init();
